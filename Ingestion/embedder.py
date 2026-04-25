cf2520ed9546b0d02561fd24f3b90c1e4f45ed01
prateekpathak <pathakyash0610@gmail.com>
2026-04-25 19:46:44 +0530

    commit

--- Ingestion/embedder.py
+++ Ingestion/embedder.py
@@ -1,8 +1,20 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
+import os
+
 def get_embedded():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True}
-    )
+    ENV = os.getenv("ENV", "local")
+
+    if ENV == "production":
+        # ightweight for deployment
+        from langchain_community.embeddings import FakeEmbeddings
+        return FakeEmbeddings(size=384)
+
+    else:
+        # real embeddings for local
+        from langchain_community.embeddings import HuggingFaceEmbeddings
+        return HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2",
+            model_kwargs={"device": "cpu"},
+            encode_kwargs={"normalize_embeddings": True}
+        )
