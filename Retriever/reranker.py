fe77eeefae57e1fe67f6b1e089b57b89407d139b
prateekpathak <pathakyash0610@gmail.com>
2026-04-25 20:24:46 +0530

    commit

--- Retriever/reranker.py
+++ Retriever/reranker.py
@@ -1,9 +1,24 @@
-from sentence_transformers import CrossEncoder
-reranker= CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
+import os
 
-def rerank_docs(query, docs):
-    pairs= [(query, d.page_content) for d in docs]
-    scores= reranker.predict(pairs)
+ENV = os.getenv("ENV", "local")
 
-    ranked= sorted(zip(docs, scores),  key= lambda x: x[1], reverse=True)
-    return [doc for doc, _ in ranked[:3]]
+#PRODUCTION (LIGHTWEIGHT)
+if ENV == "production":
+
+    def rerank_docs(llm, query, docs):
+        # simple fallback (no heavy models)
+        return docs[:3]
+
+
+#LOCAL(HIGH QUALITY)
+else:
+    from sentence_transformers import CrossEncoder
+
+    model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+    def rerank_docs(llm, query, docs):
+        pairs = [(query, doc.page_content) for doc in docs]
+        scores = model.predict(pairs)
+
+        ranked = sorted(zip(docs, scores), key=lambda x: x[1], reverse=True)
+        return [doc for doc, _ in ranked[:3]]
